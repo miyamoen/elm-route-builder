a0609c69714073450a276322ec5996891239ebeb
miyamoen <akihito.terazawa@siiibo.com>
2021-02-17 00:28:18 +0900

    long document

--- tests/RouteBuilderTest.elm
+++ tests/RouteBuilderTest.elm
@@ -59,3 +59,31 @@ toUrl paths =
 parseTest : Route a a -> List String -> a -> () -> Expectation
 parseTest r paths expect () =
     Maybe.andThen (parse (r.toParser identity)) (toUrl paths) |> equal (Just expect)
+
+
+type alias BookIds =
+    { libraryId : String, bookId : String }
+
+
+bookRoute : Route BookIds page
+bookRoute =
+    root |> s "libraries" |> string .libraryId |> s "books" |> string .bookId |> dynamic BookIds
+
+
+bookUrl : String
+bookUrl =
+    bookRoute.toString { libraryId = "図書館", bookId = "本" }
+
+
+bookParser : Url.Parser.Parser (BookPageModel -> BookPageModel) BookPageModel
+bookParser =
+    bookRoute.toParser bookPageInit
+
+
+type alias BookPageModel =
+    { ids : BookIds }
+
+
+bookPageInit : BookIds -> BookPageModel
+bookPageInit ids =
+    { ids = ids }
